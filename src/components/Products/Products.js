9f9045433d3e7c514cd4614a8279e8ddd7be2306
Kazi Saheb <kzsaheb@gmail.com>
2022-03-17 15:58:18 +0600

    first commit

---
+++ src/components/Products/Products.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './Products.css'
+function Products() {
+  const keyword = 'samsung'
+  const [phones, setPhones] = useState([])
+  useEffect(() => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${keyword}`)
+      .then(res => res.json())
+      .then(data => setPhones(data.data))
+  }, [])
+
+  return (
+    <div className='container d-flex flex-wrap justify-content-evenly'>
+      {
+        phones.map(phone => <div key={phone.slug} className='Products bg-dark text-light m-3 p-3'>
+          <img src={phone.image} alt="" />
+          <p>Brand: {phone.brand}</p>
+          <h5>Model: {phone.phone_name}</h5>
+        </div>)
+      }
+
+    </div>
+  );
+};
+
+export default Products;
